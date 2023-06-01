ab791483a5b6c86586b526727561e73b199ea16f
unknown <edmacl@hotmail.com>
2023-06-02 09:31:28 +1000

    First commit

---
+++ src/pages/ChangePassword.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import axios from 'axios';
+
+function ChangePassword() {
+
+  const [oldPassword, setOldPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+
+  const changePassword = () => {
+    axios.put("http://localhost:3001/auth/changepassword", {oldPassword: oldPassword, newPassword: newPassword}, {headers:{accessToken: localStorage.getItem("accessToken")}}).then((response) => {
+        if(response.data.error){
+            alert(response.data.error);
+        }
+    })
+  }
+  return (
+    <div>
+      <h1>Change your password</h1>
+      <input type="password" placeholder="Current password..." onChange={(event) => {setOldPassword(event.target.value)}}/>
+      <input type="password" placeholder="New password..." onChange={(event) => {setNewPassword(event.target.value)}}/>
+      <button onClick={changePassword}>Save Changes</button>
+    </div>
+  )
+}
+
+export default ChangePassword
